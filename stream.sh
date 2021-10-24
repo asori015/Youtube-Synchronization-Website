3a81949b62203d311e5ef17eb6117d2fd45e4b95
asori015 <asori015@ucr.edu>
2021-10-24 04:10:19 -0700

    Changing ports for heroku

--- stream.sh
+++ stream.sh
@@ -1,1 +1,10 @@
-ffmpeg -re -i ~/rtorrent/download/untold-history/ep9.mkv -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost:1935/live/STREAM_NAME
+ffmpeg -re -i ~/rtorrent/download/untold-history/ep9.mkv -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv \
+rtmp://localhost:5000/live/MYSTREAM
+#ffmpeg -re -i ~/rtorrent/download/untold-history/ep9.mkv -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv \
+#rtmp://not-facebook391.herokuapp.com:1935/live/MYSTREAM
+#ffmpeg -re -i ~/rtorrent/download/untold-history/ep9.mkv -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv \
+#rtmp://streamy-app.herokuapp.com/live/
+
+
+#ffmpeg -re -i ~/rtorrent/download/untold-history/ep9.mkv -vcodec libx264 -b:v 5M -acodec aac -b:a 256k -f flv \
+#rtmp://lax.contribute.live-video.net/app/live_249421641_v1GnwqvHCwvTWdb6LFY5Dy7wRDLTas
