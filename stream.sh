c4c7e22c070a6c0273b57eff5464fa4ef7af7604
asori015 <asori015@ucr.edu>
2021-10-29 05:48:51 -0700

    Heroku does not let you upload rtmp streams to its servers so the node media server will have to run on my local machine.

--- stream.sh
+++ stream.sh
@@ -1,10 +1,10 @@
-ffmpeg -re -i ~/rtorrent/download/untold-history/ep9.mkv -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv \
-rtmp://localhost:5000/live/MYSTREAM
+ffmpeg -re -i ~/rtorrent/download/Gladiator/x.mp4 -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv \
+rtmp://172.19.27.218:1935/live/MYSTREAM
 #ffmpeg -re -i ~/rtorrent/download/untold-history/ep9.mkv -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv \
-#rtmp://not-facebook391.herokuapp.com:1935/live/MYSTREAM
+#rtmp://not-facebook391.herokuapp.com:5000/live/MYSTREAM
 #ffmpeg -re -i ~/rtorrent/download/untold-history/ep9.mkv -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv \
 #rtmp://streamy-app.herokuapp.com/live/
 
 
-#ffmpeg -re -i ~/rtorrent/download/untold-history/ep9.mkv -vcodec libx264 -b:v 5M -acodec aac -b:a 256k -f flv \
+#ffmpeg -re -i ~/rtorrent/download/Gladiator/x.mp4 -vcodec libx264 -b:v 5M -acodec aac -b:a 256k -f flv \
 #rtmp://lax.contribute.live-video.net/app/live_249421641_v1GnwqvHCwvTWdb6LFY5Dy7wRDLTas
