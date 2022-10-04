e14b837f2eb81bea7130039cbaf48f3961c82c12
asori015 <asori015@ucr.edu>
2022-10-03 22:02:43 -0700

    Moved database schema into models.py. Local dev copy is now configured to use Postgres instead of sqlite, now trying to make those changes on heroku build.

--- app.py
+++ app.py
@@ -2,24 +2,20 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask.globals import request
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
-from datetime import datetime
+
+import os
 import re
-
-# import os
-# import psycopg2
-
-# DATABASE_URL = os.environ['DATABASE_URL']
-
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # flask initialization
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-# sqlite initalization
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# postgresql initalization
+#DATABASE_URL = #os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///notfacebook_dev"#os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+from models import TodoItem
 
 # socketio initialization
 app.config['SECRET_KEY'] = 'vajiralongko1232#'
@@ -33,22 +29,12 @@ currentIP = {'ip': '0.0.0.0'}           # Current IP address of livestream
 currentPlayState = {'state': False}     # Is YT video playing?
 currentSIDs = {}                        # socket IDs of currently connected clients
 
-# SQL schema
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable = False)
-    completed = db.Column(db.Integer, default = 0)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
 # index.html route
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        new_task = TodoItem(content=task_content)
 
         try:
             db.session.add(new_task)
@@ -57,14 +43,14 @@ def index():
         except:
             return 'There was an issue adding your task'
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
+        tasks = TodoItem.query.order_by(TodoItem.date_created).all()
         return render_template('index.html', tasks=tasks)
 
 # delete task route
 @app.route('/delete/<int:id>')
 def delete(id):
     print("in delete") # debug
-    task_to_delete = Todo.query.get_or_404(id)
+    task_to_delete = TodoItem.query.get_or_404(id)
 
     try:
         db.session.delete(task_to_delete)
@@ -77,7 +63,7 @@ def delete(id):
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     print("in update") # debug
-    task_to_update = Todo.query.get_or_404(id)
+    task_to_update = TodoItem.query.get_or_404(id)
 
     if request.method == 'POST':
         task_to_update.content = request.form['content']
@@ -166,4 +152,5 @@ def log_disconnect():
 
 # start server
 if __name__  == "__main__":
+    db.create_all()
     socketio.run(app, debug=True)
--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
 gunicorn --worker-class eventlet -w 1 app:app -b localhost:9999
-#python3 flaskr/server.py
